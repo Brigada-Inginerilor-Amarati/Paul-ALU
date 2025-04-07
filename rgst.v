a949491c502a12d37228e197fd66448f1dd41f23
SovietskySoldatie <vlad.iordachescu@student.upt.ro>
2025-04-07 23:48:45 +0300

    Updated code-flation

--- rgst.v
+++ rgst.v
@@ -59,25 +59,6 @@ module rgst #(
 endmodule
 
 
-// simple d flip flop
-// reset active on low
-
-module dff (
-    input  wire clk,
-    reset,
-    load_enable,
-    data_in,
-    output reg  data_out
-);
-
-    always @(posedge clk, negedge reset) begin
-        if (!reset) data_out <= 0;
-        else if (load_enable) data_out <= data_in;
-    end
-
-endmodule
-
-
 module rgst_tb;
 
     localparam width = 8;
