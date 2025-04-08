662320c583d3443239229af77b13cd8c300bd984
SovietskySoldatie <vlad.iordachescu@student.upt.ro>
2025-04-08 12:10:20 +0300

    Fixed MUXs
    I am an idiot. I brought dishonor to the project. I must commit sepuku

--- mux.v
+++ mux.v
@@ -19,19 +19,19 @@ module mux_4_to_1 (
   wire [1 : 0] data_interm;
   
   mux_2_to_1 mux0 ( // select from bits 0 and 1 based on select[0]
-     .data_in ( { data_in[0], data_in[1] } ),
+     .data_in ( { data_in[1], data_in[0] } ),
 	   .select ( select[0] ),
 	   .data_out ( data_interm[0] )
   );
   
   mux_2_to_1 mux1 ( // select from bits 2 and 3 based on select[0]
-     .data_in ( { data_in[2], data_in[3] } ),
+     .data_in ( { data_in[3], data_in[2] } ),
 	   .select ( select[0] ),
 	   .data_out ( data_interm[1] )
   );
   
   mux_2_to_1 mux_final ( // select from succesful bits based on select[1]
-     .data_in ( { data_interm[0], data_interm[1] } ),
+     .data_in ( { data_interm[1], data_interm[0] } ),
 	   .select ( select[1] ),
 	   .data_out ( data_out )
   );
