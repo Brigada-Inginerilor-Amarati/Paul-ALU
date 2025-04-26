d85a94278af9dc5866bc9f61fd0177eff99679c6
Paul Hondola <paulhondola@gmail.com>
2025-04-26 22:55:00 +0300

    updated alu.v with more signals from control unit
    Need to connect with mux-es and verify all data signals

--- ALU INTEGRATION TEST/alu.v
+++ ALU INTEGRATION TEST/alu.v
@@ -15,8 +15,8 @@ module alu (
     wire countRadix4full;
     wire countLeading0sempty;
     wire loadAregister_from_INBUS;
-	wire loadQregister_from_INBUS;
-	wire loadMregister_from_INBUS;
+    wire loadQregister_from_INBUS;
+    wire loadMregister_from_INBUS;
     wire initAregisterto0;
     wire initQandQprimregisters;
     wire initCounters;
@@ -33,6 +33,11 @@ module alu (
     wire select2Msum;
     wire selectQprimcorrection;
     wire selectQandQprimdif;
+    wire RSHIFT_signal;
+    wire LSHIFT_signal;
+    wire write_to_Qs_enable;
+    wire Q_value;
+    wire Qprim_value;
 
     //=========================================
     // COUNTERS
@@ -53,8 +58,8 @@ module alu (
         .countRadix4full(countRadix4full),
         .countLeading0sempty(countLeading0sempty),
         .loadAregister_from_INBUS(loadAregister_from_INBUS),
-		.loadQregister_from_INBUS(loadQregister_from_INBUS),
-		.loadMregister_from_INBUS(loadMregister_from_INBUS),
+        .loadQregister_from_INBUS(loadQregister_from_INBUS),
+        .loadMregister_from_INBUS(loadMregister_from_INBUS),
         .initAregisterto0(initAregisterto0),
         .initQandQprimregisters(initQandQprimregisters),
         .initCounters(initCounters),
@@ -64,6 +69,8 @@ module alu (
         .loadQregisterfromADDER(loadQregisterfromADDER),
         .increment_Radix4Counter(increment_Radix4Counter),
         .increment_SRT2Counter(increment_SRT2Counter),
+        .RSHIFT_signal(RSHIFT_signal),
+        .LSHIFT_signal(LSHIFT_signal),
         .pushAregister(pushAregister),
         .pushQregister(pushQregister),
         .select_sum_or_dif(select_sum_or_dif),
@@ -71,6 +78,9 @@ module alu (
         .select2Msum(select2Msum),
         .selectQprimcorrection(selectQprimcorrection),
         .selectQandQprimdif(selectQandQprimdif),
+        .write_to_Qs_enable(write_to_Qs_enable),
+        .Q_value(Q_value),
+        .Qprim_value(Qprim_value),
         .END(END)
     );
 
@@ -96,43 +106,90 @@ module alu (
 
     wire [8:0] A, Q, Qprim, M;
     wire [8:0] data_in_A, data_in_Q, data_in_Qprim, data_in_M;
+    wire left_shift_enable, left_shift_value, right_shift_enable, right_shift_value;
 
     rgst reg_A (
-        .clk  (clk),
+        .clk(clk),
         .reset(reset | initAregisterto0),
-        .load (loadAregister_from_INBUS | loadAregisterfromADDER),
-        .data (data_in_A),
-        .out  (A)
+        .load_enable(loadAregister_from_INBUS | loadAregisterfromADDER),
+        .left_shift_enable(LSHIFT_signal),
+        .left_shift_value(left_shift_value),
+        .right_shift_enable(RSHIFT_signal),
+        .right_shift_value(right_shift_value),
+        .data_in(data_in_A),
+        .data_out(A)
     );
 
     rgst reg_Q (
-        .clk  (clk),
+        .clk(clk),
         .reset(reset | initQandQprimregisters),
-        .load (loadQregister_from_INBUS | loadQregisterfromADDER),
-        .data (data_in_Q),
-        .out  (Q)
+        .load_enable(loadQregister_from_INBUS | loadQregisterfromADDER | write_to_Qs_enable),
+        .left_shift_enable(LSHIFT_signal),
+        .left_shift_value(Q_value),
+        .right_shift_enable(RSHIFT_signal),
+        .right_shift_value(right_shift_value),
+        .data_in(data_in_Q),
+        .data_out(Q)
     );
 
     rgst reg_Qprim (
-        .clk  (clk),
+        .clk(clk),
         .reset(reset | initQandQprimregisters),
-        .load (loadQprimregisterfromADDER),
-        .data (data_in_Qprim),
-        .out  (Qprim)
+        .load_enable(loadQprimregisterfromADDER | write_to_Qs_enable),
+        .left_shift_enable(LSHIFT_signal),
+        .left_shift_value(Qprim_value),
+        .right_shift_enable(1'b0),
+        .right_shift_value(1'b0),
+        .data_in(data_in_Qprim),
+        .data_out(Qprim)
     );
 
     rgst reg_M (
-        .clk  (clk),
+        .clk(clk),
         .reset(reset | initQandQprimregisters),
-        .load (loadMregister_from_INBUS),
-        .data (data_in_M),
-        .out  (M)
+        .load_enable(loadMregister_from_INBUS),
+        .left_shift_enable(LSHIFT_signal),
+        .left_shift_value(left_shift_value),
+        .right_shift_enable(1'b0),
+        .right_shift_value(1'b0),
+        .data_in(data_in_M),
+        .data_out(M)
     );
+
+    /*
+    rgst reg_inbus(
+        .clk(clk),
+        .reset(reset | initQandQprimregisters),
+        .load_enable(load_inbus_register_from_INBUS),
+        .left_shift_enable(LSHIFT_signal),
+        .left_shift_value(left_shift_value),
+        .right_shift_enable(1'b0),
+        .right_shift_value(1'b0),
+        .data_in(inbus),
+        .data_out()
+    );
+    */
 
     //=========================================
     // MUX FOR REGISTER INPUT
     //==========================================
 
 
+    //=========================================
+    // RCA ADDER
+    //==========================================
+
+    wire [8:0] operand_A, operand_B, adder_SUM;
+    wire adder_carry_in, adder_carry_out;
+
+    assign adder_carry_in = select_sum_or_dif;
+
+    adder_rca #(9) adder (
+        .x(operand_A),
+        .y(operand_B),
+        .carry_in(adder_carry_in),
+        .carry_out(adder_carry_out),
+        .sum(adder_SUM)
+    );
 
 endmodule
